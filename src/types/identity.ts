4240b79b3cc2b59bc59ab00169a79739746aafde
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2025-12-11 05:09:19 -0500

    [AI Expansion] Create src/types/identity.ts

---
+++ src/types/identity.ts
@@ -0,0 +1,11 @@
+export interface IdentityApp {
+  id: string;
+  displayName: string;
+  appId: string;
+  createdDateTime: string;
+  applicationType: string;
+  accountEnabled: boolean;
+  applicationVisibility: string;
+  assignmentRequired: boolean;
+  isAppProxy: boolean;
+}
