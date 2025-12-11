7a8bac1956787263f9885aa17217de8c36d0d1d2
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2025-12-11 06:14:19 -0500

    [AI Expansion] Create src/components/Wallet/ChainSelector.tsx

---
+++ src/components/Wallet/ChainSelector.tsx
@@ -0,0 +1,25 @@
+```typescript
+import React, { useState, useContext } from 'react';
+import { Select } from 'antd';
+import { ChainContext } from '../../context/ChainContext';
+
+const { Option } = Select;
+
+const ChainSelector: React.FC = () => {
+  const { chain, setChain } = useContext(ChainContext);
+
+  const handleChange = (value: string) => {
+    setChain(value);
+  };
+
+  return (
+    <Select defaultValue={chain} style={{ width: 120 }} onChange={handleChange}>
+      <Option value="mainnet">Mainnet</Option>
+      <Option value="polygon">Polygon</Option>
+      <Option value="arbitrum">Arbitrum</Option>
+    </Select>
+  );
+};
+
+export default ChainSelector;
+```
