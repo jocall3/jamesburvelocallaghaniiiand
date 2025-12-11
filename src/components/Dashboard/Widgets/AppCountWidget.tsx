5e8e97d6e1b4c6139f963ba20b151b4e9abcd677
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2025-12-11 05:09:37 -0500

    [AI Expansion] Create src/components/Dashboard/Widgets/AppCountWidget.tsx

---
+++ src/components/Dashboard/Widgets/AppCountWidget.tsx
@@ -0,0 +1,102 @@
+```typescript
+import React, { useState, useEffect } from 'react';
+import { useTheme } from '@mui/material/styles';
+import { Card, CardContent, Typography, Grid, CircularProgress } from '@mui/material';
+
+interface AppData {
+  id: string;
+  displayName: string;
+  appId: string;
+  createdDateTime: string;
+  applicationType?: string;
+  accountEnabled: boolean;
+  applicationVisibility: string;
+  assignmentRequired: boolean;
+  isAppProxy: boolean;
+}
+
+const AppCountWidget: React.FC = () => {
+  const theme = useTheme();
+  const [appData, setAppData] = useState<AppData[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [counts, setCounts] = useState<{ [key: string]: number }>({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const data = `00a67483-ea0d-4bfa-a19b-17ae35e97fe3,ADP,3f18b202-a866-4c1f-b9c0-6598390a2092,2022-05-04T08:14:29Z,Enterprise Application,True,Visible,True,False
+00b4db41-9f4b-4f95-96df-8a8436aee693,Terraform Enterprise,fd681d32-9e97-4876-a58f-05269cf838e4,2022-05-04T08:02:58Z,Enterprise Application,True,Visible,True,False
+00f21adf-4610-4184-b930-c5c5b0bd5f8a,jocall3-13-325f9500-3bd3-48fe-b130-806f56e2e7cc,2e8265ff-5066-4e5d-ae61-dbcc32816115,2022-04-10T03:23:21Z,,True,Visible,False,False
+012b9292-17c5-4ed1-a899-e831b1088a22,Microsoft Substrate Management,98db8bd6-0cc0-4e67-9de5-f187f1cd1b41,2022-04-13T22:24:44Z,Microsoft Application,True,Visible,False,False
+0174c5a1-66f3-4929-913b-dfb7afe762ab,Azure Data Factory,0947a342-ab4a-43be-93b3-b8243fc161e5,2022-04-15T14:47:15Z,Microsoft Application,True,Visible,False,False
+0208605f-08b1-492b-a0fd-ac6294e14b1f,Azure Machine Learning Singularity,607ece82-f922-494f-88b8-30effaf12214,2022-03-28T03:48:59Z,Microsoft Application,True,Visible,False,False
+0249afa8-c384-4bbd-ba62-51be0d7cce63,HealthBot-RP,6db4d6bb-6649-4dc2-84b7-0b5c6894031e,2022-04-15T14:47:58Z,Microsoft Application,True,Visible,False,False
+028daf06-3ca2-4f3f-8b6e-442b8d945daf,Microsoft.MileIQ.RESTService,b692184e-b47f-4706-b352-84b288d2d9ee,2022-04-13T22:24:42Z,Microsoft Application,True,Visible,False,False
+029bfbeb-281e-43ea-8755-73fa84943a44,Microsoft Mobile Application Management Backend,354b5b6d-abd6-4736-9f51-1be80049b91f,2022-04-13T22:24:45Z,Microsoft Application,True,Visible,False,False
+038cb5e8-15d7-4f48-9004-e6d59f9c5dd7,AzureDatabricks,2ff814a6-3304-4ab8-85cb-cd0e6f879c1d,2022-04-07T09:52:32Z,Microsoft Application,True,Visible,False,False
+039d6906-ed43-42ca-bbf4-ee91e85b4bf8,Azure Machine Learning Services,18a66f5f-dbdf-4c17-9dd7-1634712a9cbe,2022-03-28T03:48:59Z,Microsoft Application,True,Visible,False,False
+03d41fe2-06b6-43d4-b181-af146f8e3e7f,Azure Notification Service,b503eb83-1222-4dcc-b116-b98ed5216e05,2022-03-28T03:48:59Z,Microsoft Application,True,Visible,False,False
+043d80c6-801d-40db-bc46-b570b9787d6e,Application Insights API,f5c26e74-f226-4ae8-85f0-b4af0080ac9e,2022-03-28T03:48:59Z,Microsoft Application,True,Visible,False,False
+043e99f7-0f2e-4387-9b76-c970aa9791ac,CPIM Service,bb2a2e3a-c5e7-4f0a-88e0-8e01fd3fc1f4,2022-04-16T23:42:40Z,Microsoft Application,True,Visible,False,False
+05211dc7-8c79-4c6b-bce0-579fc42f535f,Azure Spring Cloud Domain-Management Dogfood,584a29b4-7876-4445-921e-71e427d4f4b3,2022-04-12T08:29:48Z,Microsoft Application,True,Visible,False,False
+0557f2e6-8457-4a58-becc-fe5993a105ac,Adobe Connect,d3e1650d-38f0-4a3c-8c2d-a064cc8d9893,2022-05-04T08:13:47Z,Enterprise Application,True,Visible,True,False
+05966bf2-394d-49d1-8950-aa3521c54051,Microsoft.Azure.CertificateRegistration,f3c21649-0979-4721-ac85-b0216b2cf413,2022-04-07T05:26:54Z,Microsoft Application,True,Visible,False,False
+07f2f721-e1f6-4d65-ae15-f6e448afea60,Outlook Online Add-in App,bc59ab01-8403-45c6-8796-ac3ef710b3e3,2022-04-13T22:24:42Z,Microsoft Application,True,Visible,False,False
+09a9d819-2ca2-41c5-acaf-97889769ee89,Compute Usage Provider,a303894e-f1d8-4a37-bf10-67aa654a0596,2022-03-28T03:48:59Z,Microsoft Application,True,Visible,False,False
+0a6d1c11-8d3d-4afd-a793-c4e8a7296c4a,Azure Managed Grafana SSO,6f2d169c-08f3-4a4c-a982-bcaf2d038c45,2022-04-15T14:46:43Z,Microsoft Application,True,Visible,False,False
+0a99424f-024a-40bd-b9be-6ae12ab502e3,PlayFab,448adbda-b8d8-4f33-a1b0-ac58cf44d4c1,2022-04-15T14:48:52Z,Microsoft Application,True,Visible,False,False
+0ab21449-f496-4f9c-b9c7-6294a97ef1df,Managed Disks Resource Provider,60e6cd67-9c8c-4951-9b3c-23c25a2169af,2022-03-28T03:48:58Z,Microsoft Application,True,Visible,False,False
+0afd5e86-eff5-4769-9dbf-b75866d280e7,AzureDataShare,799f1985-1517-4fe1-af2b-ba3d87d4996b,2022-04-07T10:13:59Z,Microsoft Application,True,Visible,False,False
+0b052270-067f-4a8b-a73e-01afa1594928,Liftr Confluent RPaaS,1448fd13-7e74-41f4-b6e3-17e485d8ac2e,2022-04-15T14:46:19Z,Microsoft Application,True,Visible,False,False
+0b5f7e78-2717-4f65-8f81-59f93971fc94,Azure Regional Service Manager,5e5e43d4-54da-4211-86a4-c6e7f3715801,2022-03-28T03:48:59Z,Microsoft Application,True,Visible,False,False
+0b95e78b-c884-4a7a-9e37-ba496672fe0b,Access IoT Hub Device Provisioning Service,0cd79364-7a90-4354-9984-6e36c841418d,2022-04-07T10:01:27Z,Microsoft Application,True,Visible,False,False
+0bf5fb00-4b35-481e-8c37-5d157d49fd6c,Azure Multi-Factor Auth Connector,1f5530b3-261a-47a9-b357-ded261e17918,2022-04-13T22:24:35Z,Microsoft Application,True,Visible,False,False
+0c4f13f3-706d-4006-9218-6ad726249dbf,MicrosoftHybridDataAADapp,621269cf-1195-44a3-a835-c613d103dd15,2022-04-15T14:47:53Z,Microsoft Application,True,Visible,False,False
+0cab0d7c-b8cc-45d4-9bff-87f3901cc131,Office 365 Exchange Online,00000002-0000-0ff1-ce00-000000000000,2022-04-13T22:24:35Z,Microsoft Application,True,Visible,False,False
+0cd21db1-ab57-4b70-a942-8be945120567,Dynamics 365 Business Central,996def3d-b36c-4153-8607-a6fd3c01b89f,2022-04-13T22:24:42Z,Microsoft Application,True,Visible,False,False
+0cde9d2b-cba6-481a-bd8d-e288774d7cbd,Office 365 SharePoint Online,00000003-0000-0ff1-ce00-000000000000,2022-04-13T22:24:35Z,Microsoft Application,False,Visible,False,False
+0d0e5cff-bb6a-48a6-a30a-d03f4eeacd7c,Cortana at Work Bing Services,22d7579f-06c2-4baa-89d2-e844486adb9d,2022-04-13T22:24:41Z,Microsoft Application,True,Visible,False,False
+0d2b9521-cd89-4c17-aae8-7bbf43ad94d0,aciapi,c5b17a4f-cc6f-4649-9480-684280a2af3a,2022-04-12T17:28:45Z,Microsoft Application,True,Visible,False,False
+0d5d08b9-2c48-4cb4-be79-88fb8df8a256,Azure Purview,73c2949e-da2d-457a-9607-fcc665198967,2022-04-15T14:48:55Z,Microsoft Application,True,Visible,False,False
+0e474b1c-b64a-4254-86d4-50b8d0f6d45b,Microsoft Office Web Apps Service,67e3df25-268a-4324-a550-0de1c7f97287,2022-04-13T22:24:41Z,Microsoft Application,True,Visible,False,False
+0eb977e2-5804-44d6-85a3-96d14a191fff,MCAPI Authorization Prod,d73f4b35-55c9-48c7-8b10-651f6f2acb2e,2022-03-04T02:36:00Z,Microsoft Application,True,Visible,False,False
+0ecec5f6-db13-4b20-b9bf-88f6dc2be5c3,Service Bus MSI App,eb070ea5-bd17-41f1-ad68-5851f6e71774,2022-04-15T14:49:16Z,Microsoft Application,True,Visible,False,False
+0f246e67-5ac7-4bbb-b47c-63476cdc3d30,Azure Analysis Services,4ac7d521-0382-477b-b0f8-7e1d95f85ca2,2022-04-15T14:45:11Z,Microsoft Application,True,Visible,False,False
+0f3f5d84-d6eb-47b6-bc79-3be3e97f551a,HIS AAD Private Clouds App,9ddb5749-5660-445d-b859-394225a7d97d,2022-04-15T14:45:43Z,Microsoft Application,True,Visible,False,False
+0f8e73e9-35eb-4c57-a681-95f96da5b49a,Fluid Relay Service,3e8f1ed5-3a6e-47d1-ad1e-b14e6b69704b,2022-04-15T14:47:23Z,Microsoft Application,True,Visible,False,False
+0f9928e5-1742-4dab-a8f4-a7f9138e40d5,Storage Resource Provider,a6aa9161-5291-40bb-8c5c-923b567bee3b,2022-03-28T03:48:59Z,Microsoft Application,True,Visible,False,False
+0fa9a81f-944b-4617-861c-bc2a6c2bcbd1,Graph Connector Service,56c1da01-2129-48f7-9355-af6d59d42766,2022-04-13T22:24:44Z,Microsoft Application,True,Visible,False,False
+1000f08e-e822-4753-bb33-a994677fcd06,Microsoft.OfficeModernCalendar,ab27a73e-a3ba-4e43-8360-8bcc717114d8,2022-04-13T22:24:41Z,Microsoft Application,True,Visible,False,False
+1042613d-4f98-49e8-88bb-79aa4089365e,ACR-Tasks-Network,62c559cd-db0c-4da0-bab2-972528c65d42,2022-03-28T03:48:59Z,Microsoft Application,True,Visible,False,False
+119c2499-a11e-4d59-8a9d-76b8966a88bb,Azure Virtual Desktop ARM Provider,50e95039-b200-4007-bc97-8d5790743a63,2022-04-07T03:10:19Z,Microsoft Application,True,Visible,False,False
+119cbfb8-3550-4afb-8c65-a28529769674,Azure Monitor for SAP Solutions,39495caf-cc21-4d03-b6b0-8c4a973cf213,2022-04-15T14:44:34Z,Microsoft Application,True,Visible,False,False
+119dde92-597e-426d-ad19-b38a86529a01,AFOI-NC-RP-PME-PROD,05cf5e27-931d-47ad-826d-cb9028d8bd7a,2022-04-15T14:48:29Z,Microsoft Application,True,Visible,False,False
+12b545be-d2e9-4bc4-b0e4-1968c484abae,Microsoft.IntelligentITDigitalTwin,dfbed8b2-492a-414e-b2f0-482534e87bc5,2022-04-15T14:47:47Z,Microsoft Application,True,Visible,False,False
+130ede23-676b-45d9-b736-27cd080bf4be,Azure AD Identity Governance - Entitlement Management,810dcf14-1858-4bf2-8134-4c369fa3235b,2022-04-13T22:24:48Z,Microsoft Application,True,Visible,False,False
+13514513-58d0-4871-b35f-b8fd83508e0c,NFV Resource Provider,328fd23b-de6e-462c-9433-e207470a5727,2022-04-06T19:41:40Z,Microsoft Application,True,Visible,False,False
+1360ee7c-fb69-4381-8579-adc83ab89360,Bot Framework Dev Portal,f3723d34-6ff5-4ceb-a148-d99dcd2511fc,2022-04-15T14:45:53Z,Microsoft Application,True,Visible,False,False
+14308268-af8f-466e-a692-97b53fe29981,IAMTenantCrawler,66244124-575c-4284-92bc-fdd00e669cea,2022-04-13T22:24:47Z,Microsoft Application,True,Visible,False,False
+1477288c-22ec-4ddf-9327-ed004a6b8d4f,VideoIndexer-BE-ARM,d9794ed7-bfec-42d8-ba8d-4d7df2a1f6dc,2022-04-15T14:50:12Z,Microsoft Application,True,Visible,False,False
+15262a46-e017-4019-a536-638a7ae1e86f,Microsoft Rights Management Services,00000012-0000-0000-c000-000000000000,2022-04-13T22:24:35Z,Microsoft Application,True,Visible,False,False
+153a25be-5772-4561-b6a9-acdf39a8e2db,Azure Backup NRP Application,9bdab391-7bbe-42e8-8132-e4491dc29cc0,2022-04-10T03:05:23Z,Microsoft Application,True,Visible,False,False
+15517c29-83b5-4e03-ae00-8f523f01e292,Office 365 Configure,aa9ecb1e-fd53-4aaa-a8fe-7a54de2c1334,2022-03-04T02:35:55Z,Microsoft Application,True,Visible,False,False
+156b02da-0a13-449e-a44e-1757cb167015,Azure Monitor Control Service,e933bd07-d2ee-4f1d-933c-3752b819567b,2022-03-28T03:49:01Z,Microsoft Application,True,Visible,False,False
+1625ae7c-2b6f-49e2-92c4-c7aa10788501,Azure HPC Workbench,5bd9995f-b6f8-4c7a-a024-e8c5eab9c85d,2022-04-15T14:48:04Z,Microsoft Application,True,Visible,False,False
+172fb8fe-7fa5-4ef2-80d5-78b758fe0ce9,Test Base for M365 - Resource Provider,f3625a3e-6360-4580-968d-fae4cabc75a0,2022-04-12T01:04:42Z,Microsoft Application,True,Visible,False,False
+1795e77a-f5c8-4f83-90b0-247f8239467a,Microsoft.ServiceBus,80a10ef9-8168-493d-abf9-3297c4ef6e3c,2022-04-15T14:49:16Z,Microsoft Application,True,Visible,False,False
+17c537b0-f0b8-4888-9e9a-346e78542549,Azure Traffic Manager and DNS,2cf9eb86-36b5-49dc-86ae-9a63135dfa8c,2022-04-06T19:41:40Z,Microsoft Application,True,Visible,False,False
+186792d2-41c3-4a08-8fc0-9318aa54508e,Custom Locations RP,bc313c14-388c-4e7d-a58e-70017303ee3b,2022-04-12T01:09:04Z,Microsoft Application,True,Visible,False,False
+18fdd139-6e86-44b3-a08a-30acbf7316f4,LexisNexis Law Schools,e5d38734-8683-4c09-9e0d-73a883c2d83d,2022-05-04T08:23:45Z,Enterprise Application,True,Visible,True,False
+1943495f-346f-4b82-a67a-0e24db869828,MarketplaceAPI ISV,20e940b3-4c77-4b0b-9a53-9e16a1b010a7,2022-04-12T00:39:13Z,Microsoft Application,True,Visible,False,False
+19562460-4a11-484d-8b82-619c7ef29112,Diagnostic Services Trusted Storage Access,562db366-1b96-45d2-aa4a-f2148cef2240,2022-03-28T03:49:01Z,Microsoft Application,True,Visible,False,False
+195ff216-c783-4ebb-99a7-7fd7520cdc45,Microsoft Azure Container Apps - Control Plane,7e3bc4fd-85a3-4192-b177-5b8bfc87f42c,2022-04-06T19:33:33Z,Microsoft Application,True,Visible,False,False
+19acdbb9-f64f-4a7c-9c88-4aaae2d82ee5,DoNotDelete-DataBoxEdgeNGatewayManagedApp,2368d027-f996-4edb-bf48-928f98f2ab8c,2022-04-15T14:46:41Z,Microsoft Application,True,Visible,False,False
+19fbbfe7-8bd8-4273-86d3-501319e749ce,Defender for Containers provisioning ARC k8s Enabled,f6ec9458-aa91-4c6e-bb3f-7b7c946c1b44,2022-04-13T05:11:00Z,Managed Identity,True,Visible,False,False
+19ffe6f1-6d63-4ae5-a1da-c535e39d3544,Intune CMDeviceService,14452459-6fa6-4ec0-bc50-1528a1a06bf0,2022-04-13T22:24:46Z,Microsoft Application,True,Visible,False,False
+1af7a79a-0d3a-450f-89e9-5bde0db7aa6e,Attestation Service,c61423b7-1d1f-430d-b444-0eee53298103,2022-04-15T14:45:21Z,Microsoft Application,True,Visible,False,False
+1b526e38-3849-48ca-bb79-8ca2c4b41ec6,Azure Cost Management XCloud,3184af01-7a88-49e0-8b55-8ecdce0aa950,2022-04-13T04:47:48Z,Microsoft Application,True,Visible,False,False
+1bccd68e-a562-46eb-bfb9-8ddae9f004c7,SSO Extension Intune,163b648b-025e-455b-9937-a7f39a65d171,2022-04-13T22:24:47Z,Microsoft Application,True,Visible,False,False
+1c9a3113-5bbb-46fd-b649-ae429cc84e9f,Signup,b4bddae8-ab25-483e-8670-df09b9f1d0ea,2022-03-04T02:35:55Z,Microsoft Application,True,Visible,False,False
+1ca99c82-51f8-4e9e-b0cf-51f6a09392a1,Azure Data Lake,e9f49c6b-5ce5-44c8-925d-015017e9f7ad,2022-04-15T14:47:10Z,Microsoft Application,True,Visible,False,False
+1cae7670-eef7-4b9d-bef8-6771c6842d6b,Azure API Management,8602e328-9b72-4f2d-a4ae-1387d013a2b3,2022-04-12T05:19:22Z,Microsoft Application,True,Visible,
