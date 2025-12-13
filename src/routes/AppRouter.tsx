8b7ba3ea0c876aa9474126b76ae584339d1758f7
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2025-12-13 15:55:22 -0500

    AI Expansion: src/routes/AppRouter.tsx

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,229 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Import all the app components (assuming they are in a similar structure)
+// This is a placeholder, you'll need to adjust the import paths based on your project structure.
+// For demonstration, we'll assume a structure like: src/apps/[AppName]/[AppName].tsx
+
+// Example imports for a few hypothetical apps:
+import App1 from '../apps/App1/App1';
+import App2 from '../apps/App2/App2';
+import App3 from '../apps/App3/App3';
+import App4 from '../apps/App4/App4';
+import App5 from '../apps/App5/App5';
+import App6 from '../apps/App6/App6';
+import App7 from '../apps/App7/App7';
+import App8 from '../apps/App8/App8';
+import App9 from '../apps/App9/App9';
+import App10 from '../apps/App10/App10';
+import App11 from '../apps/App11/App11';
+import App12 from '../apps/App12/App12';
+import App13 from '../apps/App13/App13';
+import App14 from '../apps/App14/App14';
+import App15 from '../apps/App15/App15';
+import App16 from '../apps/App16/App16';
+import App17 from '../apps/App17/App17';
+import App18 from '../apps/App18/App18';
+import App19 from '../apps/App19/App19';
+import App20 from '../apps/App20/App20';
+import App21 from '../apps/App21/App21';
+import App22 from '../apps/App22/App22';
+import App23 from '../apps/App23/App23';
+import App24 from '../apps/App24/App24';
+import App25 from '../apps/App25/App25';
+import App26 from '../apps/App26/App26';
+import App27 from '../apps/App27/App27';
+import App28 from '../apps/App28/App28';
+import App29 from '../apps/App29/App29';
+import App30 from '../apps/App30/App30';
+import App31 from '../apps/App31/App31';
+import App32 from '../apps/App32/App32';
+import App33 from '../apps/App33/App33';
+import App34 from '../apps/App34/App34';
+import App35 from '../apps/App35/App35';
+import App36 from '../apps/App36/App36';
+import App37 from '../apps/App37/App37';
+import App38 from '../apps/App38/App38';
+import App39 from '../apps/App39/App39';
+import App40 from '../apps/App40/App40';
+import App41 from '../apps/App41/App41';
+import App42 from '../apps/App42/App42';
+import App43 from '../apps/App43/App43';
+import App44 from '../apps/App44/App44';
+import App45 from '../apps/App45/App45';
+import App46 from '../apps/App46/App46';
+import App47 from '../apps/App47/App47';
+import App48 from '../apps/App48/App48';
+import App49 from '../apps/App49/App49';
+import App50 from '../apps/App50/App50';
+import App51 from '../apps/App51/App51';
+import App52 from '../apps/App52/App52';
+import App53 from '../apps/App53/App53';
+import App54 from '../apps/App54/App54';
+import App55 from '../apps/App55/App55';
+import App56 from '../apps/App56/App56';
+import App57 from '../apps/App57/App57';
+import App58 from '../apps/App58/App58';
+import App59 from '../apps/App59/App59';
+import App60 from '../apps/App60/App60';
+import App61 from '../apps/App61/App61';
+import App62 from '../apps/App62/App62';
+import App63 from '../apps/App63/App63';
+import App64 from '../apps/App64/App64';
+import App65 from '../apps/App65/App65';
+import App66 from '../apps/App66/App66';
+import App67 from '../apps/App67/App67';
+import App68 from '../apps/App68/App68';
+import App69 from '../apps/App69/App69';
+import App70 from '../apps/App70/App70';
+import App71 from '../apps/App71/App71';
+import App72 from '../apps/App72/App72';
+import App73 from '../apps/App73/App73';
+import App74 from '../apps/App74/App74';
+import App75 from '../apps/App75/App75';
+import App76 from '../apps/App76/App76';
+import App77 from '../apps/App77/App77';
+import App78 from '../apps/App78/App78';
+import App79 from '../apps/App79/App79';
+import App80 from '../apps/App80/App80';
+import App81 from '../apps/App81/App81';
+import App82 from '../apps/App82/App82';
+import App83 from '../apps/App83/App83';
+import App84 from '../apps/App84/App84';
+import App85 from '../apps/App85/App85';
+import App86 from '../apps/App86/App86';
+import App87 from '../apps/App87/App87';
+import App88 from '../apps/App88/App88';
+import App89 from '../apps/App89/App89';
+import App90 from '../apps/App90/App90';
+import App91 from '../apps/App91/App91';
+import App92 from '../apps/App92/App92';
+import App93 from '../apps/App93/App93';
+import App94 from '../apps/App94/App94';
+import App95 from '../apps/App95/App95';
+import App96 from '../apps/App96/App96';
+import App97 from '../apps/App97/App97';
+import App98 from '../apps/App98/App98';
+import App99 from '../apps/App99/App99';
+import App100 from '../apps/App100/App100';
+
+// Placeholder for a potential landing page or dashboard
+import HomePage from '../pages/HomePage';
+
+const AppRouter: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Root route, could be a dashboard or landing page */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* Routes for each of the 100 Stripe Apps */}
+        <Route path="/app1" element={<App1 />} />
+        <Route path="/app2" element={<App2 />} />
+        <Route path="/app3" element={<App3 />} />
+        <Route path="/app4" element={<App4 />} />
+        <Route path="/app5" element={<App5 />} />
+        <Route path="/app6" element={<App6 />} />
+        <Route path="/app7" element={<App7 />} />
+        <Route path="/app8" element={<App8 />} />
+        <Route path="/app9" element={<App9 />} />
+        <Route path="/app10" element={<App10 />} />
+        <Route path="/app11" element={<App11 />} />
+        <Route path="/app12" element={<App12 />} />
+        <Route path="/app13" element={<App13 />} />
+        <Route path="/app14" element={<App14 />} />
+        <Route path="/app15" element={<App15 />} />
+        <Route path="/app16" element={<App16 />} />
+        <Route path="/app17" element={<App17 />} />
+        <Route path="/app18" element={<App18 />} />
+        <Route path="/app19" element={<App19 />} />
+        <Route path="/app20" element={<App20 />} />
+        <Route path="/app21" element={<App21 />} />
+        <Route path="/app22" element={<App22 />} />
+        <Route path="/app23" element={<App23 />} />
+        <Route path="/app24" element={<App24 />} />
+        <Route path="/app25" element={<App25 />} />
+        <Route path="/app26" element={<App26 />} />
+        <Route path="/app27" element={<App27 />} />
+        <Route path="/app28" element={<App28 />} />
+        <Route path="/app29" element={<App29 />} />
+        <Route path="/app30" element={<App30 />} />
+        <Route path="/app31" element={<App31 />} />
+        <Route path="/app32" element={<App32 />} />
+        <Route path="/app33" element={<App33 />} />
+        <Route path="/app34" element={<App34 />} />
+        <Route path="/app35" element={<App35 />} />
+        <Route path="/app36" element={<App36 />} />
+        <Route path="/app37" element={<App37 />} />
+        <Route path="/app38" element={<App38 />} />
+        <Route path="/app39" element={<App39 />} />
+        <Route path="/app40" element={<App40 />} />
+        <Route path="/app41" element={<App41 />} />
+        <Route path="/app42" element={<App42 />} />
+        <Route path="/app43" element={<App43 />} />
+        <Route path="/app44" element={<App44 />} />
+        <Route path="/app45" element={<App45 />} />
+        <Route path="/app46" element={<App46 />} />
+        <Route path="/app47" element={<App47 />} />
+        <Route path="/app48" element={<App48 />} />
+        <Route path="/app49" element={<App49 />} />
+        <Route path="/app50" element={<App50 />} />
+        <Route path="/app51" element={<App51 />} />
+        <Route path="/app52" element={<App52 />} />
+        <Route path="/app53" element={<App53 />} />
+        <Route path="/app54" element={<App54 />} />
+        <Route path="/app55" element={<App55 />} />
+        <Route path="/app56" element={<App56 />} />
+        <Route path="/app57" element={<App57 />} />
+        <Route path="/app58" element={<App58 />} />
+        <Route path="/app59" element={<App59 />} />
+        <Route path="/app60" element={<App60 />} />
+        <Route path="/app61" element={<App61 />} />
+        <Route path="/app62" element={<App62 />} />
+        <Route path="/app63" element={<App63 />} />
+        <Route path="/app64" element={<App64 />} />
+        <Route path="/app65" element={<App65 />} />
+        <Route path="/app66" element={<App66 />} />
+        <Route path="/app67" element={<App67 />} />
+        <Route path="/app68" element={<App68 />} />
+        <Route path="/app69" element={<App69 />} />
+        <Route path="/app70" element={<App70 />} />
+        <Route path="/app71" element={<App71 />} />
+        <Route path="/app72" element={<App72 />} />
+        <Route path="/app73" element={<App73 />} />
+        <Route path="/app74" element={<App74 />} />
+        <Route path="/app75" element={<App75 />} />
+        <Route path="/app76" element={<App76 />} />
+        <Route path="/app77" element={<App77 />} />
+        <Route path="/app78" element={<App78 />} />
+        <Route path="/app79" element={<App79 />} />
+        <Route path="/app80" element={<App80 />} />
+        <Route path="/app81" element={<App81 />} />
+        <Route path="/app82" element={<App82 />} />
+        <Route path="/app83" element={<App83 />} />
+        <Route path="/app84" element={<App84 />} />
+        <Route path="/app85" element={<App85 />} />
+        <Route path="/app86" element={<App86 />} />
+        <Route path="/app87" element={<App87 />} />
+        <Route path="/app88" element={<App88 />} />
+        <Route path="/app89" element={<App89 />} />
+        <Route path="/app90" element={<App90 />} />
+        <Route path="/app91" element={<App91 />} />
+        <Route path="/app92" element={<App92 />} />
+        <Route path="/app93" element={<App93 />} />
+        <Route path="/app94" element={<App94 />} />
+        <Route path="/app95" element={<App95 />} />
+        <Route path="/app96" element={<App96 />} />
+        <Route path="/app97" element={<App97 />} />
+        <Route path="/app98" element={<App98 />} />
+        <Route path="/app99" element={<App99 />} />
+        <Route path="/app100" element={<App100 />} />
+
+        {/* Add a catch-all route for 404 errors if desired */}
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
